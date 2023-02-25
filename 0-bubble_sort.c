95a0c4cc118435586dd6ce183043c448765436c3
BM-Daniel <danbabereyir@gmail.com>
2023-02-25 15:42:45 -0800

    Fix error in task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,27 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Write a function that sorts an array of integers in
- * ascending order using the Bubble sort algorithm
- *
- * @array: Pointer to array
- * @size: The size of the array
- */
-
+  * bubble_sort - A function for bubble sorting.
+  * @array: The array to sort.
+  * @size: THe length of the aaray.
+  * Return: Nothing.
+  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+	size_t i = 0, j = 0;
+	int aux = 0;
 
 	if (array == NULL || size == 0)
 		return;
-
-	for (i = 0; i < size - 1; i++)
+	for (; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j + 1];
+				aux = array[j + 1];
 				array[j + 1] = array[j];
-				array[j] = temp;
+				array[j] = aux;
 				print_array(array, size);
 			}
 		}
